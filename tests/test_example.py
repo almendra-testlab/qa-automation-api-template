b47a542337a7c2bc506f92ada0d8d24c6b6edfab
Almendra Polezzeli <apolezzeli@gmail.com>
2026-02-15 01:34:09 -0300

    test: parametrize endpoint tests to improve coverage and scalability

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,29 +1,21 @@
+import pytest
 from utils.api_client import APIClient
 from config.settings import BASE_URL
 
 
-def test_get_posts():
+@pytest.mark.parametrize("endpoint", [
+    "/posts",
+    "/comments",
+    "/albums",
+])
+def test_endpoints_return_data(endpoint):
     client = APIClient(BASE_URL)
-    response = client.get("/posts")
+    response = client.get(endpoint)
 
     assert response.status_code == 200
 
     data = response.json()
 
-    # validar que hay datos
     assert isinstance(data, list)
     assert len(data) > 0
 
-    # validar estructura del primer post
-    first_post = data[0]
-
-    assert "userId" in first_post
-    assert "id" in first_post
-    assert "title" in first_post
-    assert "body" in first_post
-
-    # validar tipos
-    assert isinstance(first_post["userId"], int)
-    assert isinstance(first_post["id"], int)
-    assert isinstance(first_post["title"], str)
-    assert isinstance(first_post["body"], str)
